84cf96f7c29190e043b9ce48682e225dae4c4da8
Heavenstone <malolegendrelemaire@gmail.com>
2020-12-04 20:33:17 +0100

    FIREBASE WORK PLEASE

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -3,11 +3,12 @@ import type { Chat, Message, User } from '~/utils/dbTypes'
 
 const fromB64 = (t: string) => Buffer.from(t, 'base64').toString()
 
-const creds = JSON.parse(fromB64(process.env.GCLOUD_CREDENTIALS as string))
-console.log(creds)
 !admin.apps.length
   ? admin.initializeApp({
-      credential: creds,
+      credential: admin.credential.cert(
+        JSON.parse(fromB64(process.env.GCLOUD_CREDENTIALS as string))
+      ),
+      databaseURL: 'https://nychat-7f56f.firebaseio.com',
     })
   : admin.app()
 
