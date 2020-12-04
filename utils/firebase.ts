86978e789e230fc607ce1e17bb84bee6daefc7aa
Heavenstone <malolegendrelemaire@gmail.com>
2020-12-04 21:06:24 +0100

    base64 firebase creds

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,9 +1,11 @@
 import * as admin from 'firebase-admin'
 import type { Chat, Message, User } from '~/utils/dbTypes'
 
+const fromB64 = (t: string) => Buffer.from(t, 'base64').toString()
+
 !admin.apps.length
   ? admin.initializeApp({
-      credential: JSON.parse(process.env.FIREBASE_CREDS as string),
+      credential: JSON.parse(fromB64(process.env.FIREBASE_CREDS as string)),
       databaseURL: 'https://nychat-7f56f.firebaseio.com',
     })
   : admin.app()
