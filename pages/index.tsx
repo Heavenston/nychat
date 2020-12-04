7ef0ed4bf0a98d77ee3fad508a40d2f0ba6688c8
Heavenstone <malolegendrelemaire@gmail.com>
2020-12-04 14:38:16 +0100

    removed join chat form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import DefaultLayout from '../layouts/default'
-import JoinChatForm from '../components/joinChatForm'
-import CreateChatForm from '../components/createChatForm'
 
 export default function Home() {
   return (
     <DefaultLayout>
       <div className='flex-grow flex-shrink flex flex-wrap gap-8 justify-center content-center items-center'>
-        <JoinChatForm />
-        <CreateChatForm />
+        <form className='max-w-screen flex-grow-0 block min-w-80 px-10 p-8 rounded-md shadow-lg bg-gray-100 dark:bg-gray-900'>
+          <h3 className='mb-5 text-lg font-semibold border-black dark:border-white border-solid border-b-2'>
+            Create a chat
+          </h3>
+          <label className='flex items-center mb-3'>
+            <span className='mr-3 whitespace-nowrap'>Chat Name</span>
+            <input
+              className='min-w-0 form-input flex-shrink flex-grow mt-1 bg-white dark:bg-gray-800 rounded-lg'
+              placeholder='A cool name'
+            />
+          </label>
+          <button className='rounded text-lg text-center p-2 px-4 block w-full bg-white hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-600'>
+            Create
+          </button>
+        </form>
       </div>
     </DefaultLayout>
   )
