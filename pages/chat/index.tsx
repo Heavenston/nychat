d69ccdab8d4dedf3e6f05609589d58bac1004e25
Heavenstone <malolegendrelemaire@gmail.com>
2020-12-05 20:45:10 +0100

    User list !

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -4,6 +4,9 @@ import Message from '~/components/message'
 import { useState, FormEvent, useEffect, useMemo, useRef } from 'react'
 import { useRouter } from 'next/router'
 import { stringify } from 'querystring'
+import { mdiAccount, mdiCrown, mdiDelete, mdiLink, mdiPlus } from '@mdi/js'
+import { Icon } from '@mdi/react'
+import copy from 'copy-to-clipboard'
 
 interface CurrentUser {
   id: string
@@ -118,46 +121,170 @@ export default function ChatIndex() {
     setCurrentMessage('')
   }
 
+  const deleteUser = (id: string) => () => {
+    fetch(`/api/user/${id}`, {
+      method: 'DELETE',
+      headers: {
+        authorization: currentUser?.secret || '',
+      },
+    }).then(async r => {
+      if (!r.ok) {
+        console.log(await r.json())
+        return
+      }
+      currentUser?.chat.users.splice(
+        currentUser?.chat.users.findIndex(u => u.id === id) || 0,
+        1
+      )
+      currentUser && setCurrentUser({ ...currentUser })
+    })
+  }
+  const createUser = () => {
+    fetch(`/api/user`, {
+      method: 'POST',
+      headers: {
+        authorization: currentUser?.secret || '',
+      },
+    }).then(async r => {
+      if (!r.ok) {
+        console.log(await r.json())
+        return
+      }
+      const { id, name, admin } = await r.json()
+      currentUser?.chat.users.push({
+        admin,
+        id,
+        name,
+      })
+      currentUser && setCurrentUser({ ...currentUser })
+    })
+  }
+  const copyLink = (id: string) => async () => {
+    const url = new URL(location.href)
+    const r = await fetch(`/api/user/${id}`, {
+      method: 'GET',
+      headers: {
+        authorization: currentUser?.secret || '',
+      },
+    })
+    const { secret } = await r.json()
+    url.pathname = `/chat/${secret}`
+    copy(url.toString())
+  }
+
   return (
     <DefaultLayout path_name={`${currentUser?.chat.name || ''}`}>
       <div className='flex justify-center items-center flex-grow p-2 md:p-8 xl:px-64'>
-        <div className='w-full h-full bg-gray-200 shadow dark:bg-gray-700 rounded overflow-hidden flex flex-col p-3 gap-3'>
-          <div
-            className='flex-grow flex-shrink-0 px-2 overflow-y-scroll relative'
-            ref={messageRef}
-          >
-            <div className='absolute'>
-              {Object.entries(messages)
-                .sort(([, a], [, b]) => a.date.getTime() - b.date.getTime())
-                .map(([id, m]) => (
-                  <Message
-                    key={id}
-                    sentDate={m.date}
-                    authorName={
-                      currentUser?.chat.users.find(u => u.id === m.author)
-                        ?.name || 'ERROR'
-                    }
-                    content={m.content}
-                    className='mb-2'
-                  />
-                ))}
+        <div className='w-full h-full bg-gray-200 shadow dark:bg-gray-700 rounded overflow-hidden flex p-3 gap-3'>
+          <div className='flex flex-col gap-3 flex-grow'>
+            <div
+              className='flex-grow flex-shrink-0 px-2 overflow-y-scroll relative'
+              ref={messageRef}
+            >
+              <div className='absolute'>
+                {Object.entries(messages)
+                  .sort(([, a], [, b]) => a.date.getTime() - b.date.getTime())
+                  .map(([id, m]) => (
+                    <Message
+                      key={id}
+                      sentDate={m.date}
+                      authorName={
+                        currentUser?.chat.users.find(u => u.id === m.author)
+                          ?.name || 'ERROR'
+                      }
+                      content={m.content}
+                      className='mb-2'
+                    />
+                  ))}
+              </div>
+            </div>
+            <form
+              onSubmit={sendMessage}
+              className='flex flex-grow-0 flex-shrink-0 gap-3'
+            >
+              <input
+                value={currentMessage}
+                onChange={e => {
+                  setCurrentMessage(e.target.value)
+                }}
+                className='form-input flex-grow rounded bg-gray-300 dark:bg-gray-600'
+              />
+              <button className='rounded focus:outline-none focus:ring px-10 text-center align-middle leading-full bg-gray-300 dark:bg-gray-600'>
+                Send
+              </button>
+            </form>
+          </div>
+          <div className='w-52 relative overflow-hidden overflow-y-scroll'>
+            <div className='absolute inset-0 flex flex-col gap-3 p-2'>
+              {currentUser?.admin ? (
+                <>
+                  {currentUser?.chat.users.map(user => (
+                    <div
+                      key={user.name}
+                      className='p-2 px-3 dark:hover:bg-gray-600 rounded flex items-center'
+                    >
+                      <span>{user.name}</span>
+                      {user.id === currentUser?.id && (
+                        <span>
+                          <Icon path={mdiAccount} size={0.8} />
+                        </span>
+                      )}
+                      {user.admin && (
+                        <span className='px-1'>
+                          <Icon
+                            path={mdiCrown}
+                            size={0.9}
+                            className='text-yellow-300 inline'
+                          />
+                        </span>
+                      )}
+                      <span className='flex-grow' />
+                      <button
+                        onClick={copyLink(user.id)}
+                        className='focus:outline-none focus:ring text-gray-300 flex-shrink hover:text-white'
+                      >
+                        <Icon path={mdiLink} size={0.9} />
+                      </button>
+                      {user.id !== currentUser?.id && (
+                        <button
+                          onClick={deleteUser(user.id)}
+                          className='focus:outline-none focus:ring ml-1 text-gray-300 flex-shrink hover:text-red-400'
+                        >
+                          <Icon path={mdiDelete} size={0.9} />
+                        </button>
+                      )}
+                    </div>
+                  ))}
+                  <button
+                    onClick={() => createUser()}
+                    className='focus:outline-none focus:ring p-2 flex justify-center items-center cursor-pointer dark:hover:bg-gray-600 rounded'
+                  >
+                    <Icon path={mdiPlus} size={1} />
+                  </button>
+                </>
+              ) : (
+                <>
+                  {currentUser?.chat.users.map(user => (
+                    <div key={user.name} className='p-2 px-3 rounded'>
+                      {user.name}
+                      {user.id === currentUser?.id && (
+                        <Icon path={mdiAccount} size={0.8} className='inline' />
+                      )}
+                      {user.admin && (
+                        <span className='px-1'>
+                          <Icon
+                            path={mdiCrown}
+                            size={0.9}
+                            className='text-yellow-300 inline'
+                          />
+                        </span>
+                      )}
+                    </div>
+                  ))}
+                </>
+              )}
             </div>
           </div>
-          <form
-            onSubmit={sendMessage}
-            className='flex flex-grow-0 flex-shrink-0 gap-3'
-          >
-            <input
-              value={currentMessage}
-              onChange={e => {
-                setCurrentMessage(e.target.value)
-              }}
-              className='form-input flex-grow rounded bg-gray-300 dark:bg-gray-600'
-            />
-            <button className='rounded focus:outline-none focus:ring px-10 text-center align-middle leading-full bg-gray-300 dark:bg-gray-600'>
-              Send
-            </button>
-          </form>
         </div>
       </div>
     </DefaultLayout>
