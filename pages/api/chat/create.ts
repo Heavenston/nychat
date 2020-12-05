e708dbc66d1575ae941162827652a3d4e8560a99
Heavenstone <malolegendrelemaire@gmail.com>
2020-12-05 11:10:23 +0100

    Chat creation !

--- pages/api/chat/create.ts
+++ pages/api/chat/create.ts
@@ -1,25 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { chatsCollection, usersCollection } from '~/utils/firebase'
 import { generateId } from '~/utils/idGenerator'
+import { firestore } from 'firebase-admin'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') return
-  if (typeof req.body !== 'object') return
+  if (req.method !== 'POST') {
+    res.status(404).end()
+    return
+  }
+  if (typeof req.body !== 'object') {
+    res.status(400).json({
+      message: 'Invalid body',
+    })
+    return
+  }
 
   const { name } = req.body
-  if (typeof name !== 'string') return
+  if (typeof name !== 'string') {
+    res.status(400).json({
+      message: 'Invalid chat name',
+    })
+  }
 
   const chatId = generateId()
   const adminUserId = generateId()
   const adminUserSecret = generateId(6)
 
   const adminUserRef = usersCollection.doc(adminUserId)
-  adminUserRef.create({
+  await adminUserRef.create({
     name: 'User #1',
     secret: adminUserSecret,
     admin: true,
     chat: chatsCollection.doc(chatId),
-    date: FirebaseFirestore.Timestamp.now(),
+    date: firestore.Timestamp.now(),
   })
   const adminUser = await adminUserRef.get()
   const adminUserData = adminUser.data()
@@ -28,26 +41,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   await chatRef.create({
     name,
     users: [adminUserRef],
-    date: FirebaseFirestore.Timestamp.now(),
+    date: firestore.Timestamp.now(),
   })
-  const chat = await chatRef.get()
-  const chatData = chat.data()
-  if (chatData === undefined) throw ''
 
   res.status(200).json({
     user: {
       id: adminUserRef.id.toString(),
       name: adminUserData.name,
       admin: adminUserData.admin ?? false,
-    },
-    chat: {
-      id: chatRef.id,
-      name: chatData.name,
-      users: (await Promise.all(chatData.users.map(u => u.get()))).map(u => ({
-        id: u.id,
-        name: u.data()?.name,
-        admin: u.data()?.admin ?? false,
-      })),
     },
     userSecret: adminUserSecret,
   })
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
-import { FormEvent } from 'react'
+import { useRouter } from 'next/router'
+import { FormEvent, useState } from 'react'
 import DefaultLayout from '~/layouts/default'
 
 export default function Home() {
+  const router = useRouter()
+  const [chatName, setChatName] = useState('')
+  const [loading, setLoading] = useState(false)
+
   const onFormSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    ;(async () => {
+      e.preventDefault()
+      if (loading) return
+      if (chatName.length > 2 && chatName.length < 20) {
+        setChatName('')
+      }
+      setLoading(true)
+      const r = await fetch('/api/chat/create', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: chatName,
+        }),
+      })
+      if (!r.ok) {
+        console.error(r.json())
+        return
+      }
+      const { user, userSecret } = await r.json()
+      localStorage.setItem('user', JSON.stringify(user))
+      localStorage.setItem('secret', userSecret)
+      router.push('/chat')
+    })()
   }
 
   return (
@@ -19,6 +48,9 @@ export default function Home() {
           <label className='flex items-center mb-3'>
             <span className='mr-3 whitespace-nowrap'>Chat Name</span>
             <input
+              value={chatName}
+              maxLength={20}
+              onChange={e => setChatName(e.target.value)}
               className='min-w-0 form-input flex-shrink flex-grow mt-1 bg-white dark:bg-gray-800 rounded-lg'
               placeholder='A cool name'
             />
